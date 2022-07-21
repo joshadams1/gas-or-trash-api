ae739890c12a43de0ce2ffe4b6213134421b839c
Joshua Adams <joshuaadams@Joshuas-MacBook-Pro.local>
2022-07-21 14:30:55 -0700

    Attempt to fix cors issues again

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ require('dotenv').config();
 
 const app = express();
 const dataFile = path.join(__dirname, 'data.json');
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.urlencoded({extended: true}));
 
